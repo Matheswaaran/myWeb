18b83f943d2105423f54a1c74b34d6de814e26cb
matheswaaran <matheswaarans@gmail.com>
2018-11-28 16:56:37 +0530

    Added: gatsby-config.js icon.png gatsby-plugin-react-helmet plugin

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    siteMetadata: {
+        title: `All About Matheswaaran`,
+        siteUrl: `https://matheswaaran.com`,
+        author: `Matheswaaran`,
+        description: `A small webpage about myself and my works - Matheswaaran`,
+        keywords: `matheswaaran,Resume,cv,website`,
+    },
+    plugins: [
+        'gatsby-plugin-react-helmet'
+    ],
+};
