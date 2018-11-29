7bc4c885de42bcd92413ab6620ca08203dd4d288
matheswaaran <matheswaarans@gmail.com>
2018-11-29 16:33:20 +0530

    Fixed: Width of Intro Header Modified: Nacigation bar completed

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import '../css/NavigationBar.css';
+import { FaFacebookF, FaBars, FaTwitter, FaGoogle, FaInstagram, FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { MdClose } from "react-icons/md";
 
 class NavigationBar extends React.Component {
 
@@ -17,9 +19,15 @@ class NavigationBar extends React.Component {
             <div>
                 <div className="menu-responsive">
                     <a href="#home">MATHESWAARAN</a>
-                    <a className="icon" onClick={this.onIconCLickHandler}>
-                        {this.state.responsive ? (<i className="fa fa-close"/>) : (<i className="fa fa-bars"/>)}
-                    </a>
+                    {this.state.responsive ?
+                        (<a className="icon" onClick={this.onIconCLickHandler}>
+                            <MdClose size={30} color="#FFF"/>
+                        </a>)
+                    :
+                        (<a className="icon" onClick={this.onIconCLickHandler}>
+                            <FaBars size={30} color="#FFF"/>
+                        </a>)
+                    }
                 </div>
                 <header className={this.state.responsive ? "header-container responsive" : "header-container"}>
                     <nav id="menu" className="navbar navbar-default">
@@ -39,16 +47,16 @@ class NavigationBar extends React.Component {
                     <div className="menu-footer">
                         <div className="social">
                             <ul>
-                                <li><a href="https://www.facebook.com/matheswaaran" target="_blank" rel="noopener noreferrer"><i className="fa fa-facebook"/></a></li>
-                                <li><a href="https://twitter.com/Matheswaaran_S" target="_blank" rel="noopener noreferrer"><i className="fa fa-twitter"/></a></li>
-                                <li><a href="https://plus.google.com/u/0/+Matheswaaran" target="_blank" rel="noopener noreferrer"><i className="fa fa-google-plus"/> </a></li>
-                                <li><a href="https://www.linkedin.com/in/matheswaaran" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin"/> </a></li>
-                                <li><a href="https://www.instagram.com/matheswaaran/" target="_blank" rel="noopener noreferrer"><i className="fa fa-instagram"/> </a></li>
-                                <li><a href="https://www.github.com/Matheswaaran" target="_blank" rel="noopener noreferrer"><i className="fa fa-github"/> </a></li>
+                                <li><a href="https://www.facebook.com/matheswaaran" target="_blank" rel="noopener noreferrer"><FaFacebookF className="icon" size={14}/></a></li>
+                                <li><a href="https://twitter.com/Matheswaaran_S" target="_blank" rel="noopener noreferrer"><FaTwitter className="icon" size={14}/></a></li>
+                                <li><a href="https://plus.google.com/u/0/+Matheswaaran" target="_blank" rel="noopener noreferrer"><FaGoogle className="icon" size={14}/></a></li>
+                                <li><a href="https://www.linkedin.com/in/matheswaaran" target="_blank" rel="noopener noreferrer"><FaLinkedinIn className="icon" size={14}/></a></li>
+                                <li><a href="https://www.instagram.com/matheswaaran/" target="_blank" rel="noopener noreferrer"><FaInstagram className="icon" size={14}/></a></li>
+                                <li><a href="https://www.github.com/Matheswaaran" target="_blank" rel="noopener noreferrer"><FaGithub className="icon" size={14}/></a></li>
                             </ul>
                         </div>
                         <div className="copyright">
-                            <p>©Matheswaaran all rights reserved</p>
+                            <p>&copy; Matheswaaran <br/>all right reserved</p>
                         </div>
                     </div>
                 </header>
