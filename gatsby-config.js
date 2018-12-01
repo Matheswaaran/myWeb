2d1d21f386026148212f94fcb9fa768952ff15fd
matheswaaran <matheswaarans@gmail.com>
2018-12-01 13:34:25 +0530

    Added: Graphql for IntroHeader, NavigationBar, AboutMe

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,15 @@ module.exports = {
         keywords: `matheswaaran,Resume,cv,website`,
     },
     plugins: [
-        'gatsby-plugin-react-helmet'
+        'gatsby-plugin-react-helmet',
+        {
+            resolve: 'gatsby-source-filesystem',
+            options: {
+                path: `${__dirname}/src/Data`,
+                name: 'markdown-data'
+            }
+        },
+        'gatsby-transformer-remark',
+        'gatsby-transformer-json',
     ],
 };
