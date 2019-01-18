940a42593278852038a3eeb120a93b3d00c35ad6
matheswaaran <matheswaarans@gmail.com>
2019-01-18 19:13:57 +0530

    Added: gatsby-node.js blog template page

---
+++ gatsby-node.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+  const blogTemplate = path.resolve('src/templates/SingleBlogPost.js');
+
+  return graphql(`
+    {
+      allMarkdownRemark {
+        edges{
+          node{
+            frontmatter{ path title author sub_title date category }
+            timeToRead
+            html
+            excerpt
+            id
+          }
+        }
+      }
+    }
+  `).then(res => {
+    if (res.errors){
+      return Promise.reject(res.errors)
+    }
+    res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: node.frontmatter.path,
+        component: blogTemplate
+      })
+    });
+  });
+};
