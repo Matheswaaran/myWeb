87de31eb8fb218ea0a807b09efb9687f37b04579
matheswaarans <mat@nfnlabs.in>
2019-05-01 23:15:39 +0530

    Fixed: Images in blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,50 +1,46 @@
 import React from 'react';
 import '../css/Blog.css';
-import {graphql} from "gatsby";
-import { ScrollingProvider, Section } from "react-scroll-section";
-import Layout from "../components/Layout";
-import BlogPostSingle from "../components/BlogPostSingle";
-import Contact from "../components/Contact";
-import Breadcrumb from "../components/Breadcrumb";
+import { graphql } from 'gatsby';
+import { ScrollingProvider, Section } from 'react-scroll-section';
+import Layout from '../components/Layout';
+import BlogPostSingle from '../components/BlogPostSingle';
+import Contact from '../components/Contact';
+import Breadcrumb from '../components/Breadcrumb';
 
-const Blog = ({ data }) => {
-  return(
-    <ScrollingProvider scrollBehavior="smooth">
-      <Layout pageTitle="My Blog Posts" blogPage>
-        <Breadcrumb
-          title="Blog Page"
-          subTitle="Matheswaaran's Blog"
-        />
-        <Section id="recent_posts">
-          <section className="blog blog-page white-bg page-section">
-            <div className="container-fluid">
-              <div className="row">
-                {data.allMarkdownRemark.edges.map((node, index) => {
-                  return(
-                    <BlogPostSingle
-                      key={index}
-                      frontmatter={node.node.frontmatter}
-                      excerpt={node.node.excerpt}
-                      timeToRead={node.node.timeToRead}
-                      image={data.allImageSharp.edges[index].node.fixed.src}
-                    />
-                  );
-                })}
-              </div>
+const Blog = ({ data }) => (
+  <ScrollingProvider scrollBehavior="smooth">
+    <Layout pageTitle="My Blog Posts" blogPage>
+      <Breadcrumb
+        title="Blog Page"
+        subTitle="Matheswaaran's Blog"
+      />
+      <Section id="recent_posts">
+        <section className="blog blog-page white-bg page-section">
+          <div className="container-fluid">
+            <div className="row">
+              {data.allMarkdownRemark.edges.map((node, index) => (
+                <BlogPostSingle
+                  key={`${index + 1}`}
+                  frontmatter={node.node.frontmatter}
+                  excerpt={node.node.excerpt}
+                  timeToRead={node.node.timeToRead}
+                  image={data.allImageSharp.edges[index].node.fixed.src}
+                />
+              ))}
             </div>
-          </section>
-        </Section>
-        <Section id="contact">
-          <Contact
-            socialLinks={data.dataJson.socialLinks}
-            resumeUrl={data.dataJson.introHeader.resumeUrl}
-            contact={data.dataJson.contact}
-          />
-        </Section>
-      </Layout>
-    </ScrollingProvider>
-  );
-};
+          </div>
+        </section>
+      </Section>
+      <Section id="contact">
+        <Contact
+          socialLinks={data.dataJson.socialLinks}
+          resumeUrl={data.dataJson.introHeader.resumeUrl}
+          contact={data.dataJson.contact}
+        />
+      </Section>
+    </Layout>
+  </ScrollingProvider>
+);
 
 export const query = graphql`
   query BlogIndexQuery {
